eb1cdb703b05b5409bfad2d2895db311b04845c1
Umar Farooq <ufservices465@gmail.com>
2024-06-15 10:55:17 +0530

    revert changes

--- hostel_management/hotel_management/wsgi.py
+++ hostel_management/hotel_management/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hostel_management.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_management.settings')
 
 application = get_wsgi_application()
 
